cf99a0a0e244e79ca513a4c49f8a3fee925f31b9
danielmarbach <daniel.marbach@openplace.net>
2017-09-29 15:38:27 +0200

    Really basic request response example

---
+++ request-response/Back/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrdersController : Controller
+    {
+        private static int numberOfCalls;
+
+        // PUT api/orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id)
+        {
+            await Task.Delay(1000);
+
+            if(Interlocked.Increment(ref numberOfCalls) % 2 == 0)
+            {
+                return BadRequest();
+            }
+            return Accepted();
+        }
+    }
+}
---
+++ request-response/Front/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Front.Models;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+
+namespace Front.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const int MaxQueryRetryCount = 20;
+
+        private static readonly Uri serviceUri;
+        private static readonly TimeSpan backoffQueryDelay;
+
+        private static readonly FabricClient fabricClient;
+
+        private static readonly OrderBackendClientFactory communicationFactory;
+
+        static Random random = new Random();
+
+        static HomeController()
+        {
+            serviceUri = new Uri(FabricRuntime.GetActivationContext().ApplicationName + "/Back");
+
+            backoffQueryDelay = TimeSpan.FromSeconds(3);
+
+            fabricClient = new FabricClient();
+
+            communicationFactory = new OrderBackendClientFactory(new ServicePartitionResolver(() => fabricClient));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Order()
+        {
+            var partitionClient = new ServicePartitionClient<OrderBackendClient>(communicationFactory, serviceUri);
+            await partitionClient.InvokeWithRetryAsync(client => client.Order(random.Next()));
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
