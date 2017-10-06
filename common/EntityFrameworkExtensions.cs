540b2c888e2ecbc4bd3260f13617c132caf85a14
Bob Langley <bob.langley@particular.net>
2017-10-06 15:22:18 -0700

    Move EntityFrameworkExtensions to common folder and make generic

---
+++ common/EntityFrameworkExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Fabric;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class EntityFrameworkExtensions
+{
+    public static void AddEntityFramework<T>(this IServiceCollection services) where T: DbContext
+    {
+        var provider = services.BuildServiceProvider();
+        var context = provider.GetService<StatelessServiceContext>();
+        var configurationPackage = context.CodePackageActivationContext.GetConfigurationPackageObject("Config");
+        var connectionString = configurationPackage.Settings.Sections["SqlServer"].Parameters["ConnectionString"];
+
+        services.AddDbContextPool<T>(o => o.UseSqlServer(connectionString.Value));
+    }
+}
